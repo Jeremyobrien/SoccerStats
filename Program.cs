5c7258c53d995469bb5b923c386c935837ec2a03
@Jeremyobrien <jjob1391@gmail.com>
2022-07-21 14:19:22 -0400

    Add data parsing logic

--- Program.cs
+++ Program.cs
@@ -14,12 +14,7 @@ namespace SoccerStats
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            var fileContents = ReadFile(fileName);
-            string[] fileLines = fileContents.Split(new char[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
-            foreach( var line in fileLines)
-            {
-                Console.WriteLine(line);
-            }
+            var fileContents = ReadSoccerResults(fileName);
 
             }
         public static string ReadFile(string fileName)
@@ -28,6 +23,59 @@ namespace SoccerStats
             {
                 return reader.ReadToEnd();
             }
+        }
+        public static List<GameResult> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<GameResult>();
+            using (var reader = new StreamReader(fileName))
+            {
+               string line = "";
+               reader.ReadLine();
+               while((line = reader.ReadLine()) != null)
+                {
+                    var gameResult = new GameResult();
+                    string[] values = line.Split(',');
+                    DateTime gameDate;
+                    if (DateTime.TryParse(values[0], out gameDate))
+                    {
+                        gameResult.GameDate = gameDate;
+                    }
+                    gameResult.TeamName = values[1];
+                    HomeOrAway homeOrAway;
+                    if (Enum.TryParse(values[2], out homeOrAway))
+                    {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+                    soccerResults.Add(gameResult);
+                    int parseInt;
+                    if (int.TryParse(values[3], out parseInt))
+                    {
+                        gameResult.Goals = parseInt;
+                    }
+                    if (int.TryParse(values[4], out parseInt))
+                    {
+                        gameResult.GoalAttempts = parseInt;
+                    }
+                    if (int.TryParse(values[5], out parseInt))
+                    {
+                        gameResult.ShotsOnGoal = parseInt;
+                    }
+                    if (int.TryParse(values[6], out parseInt))
+                    {
+                        gameResult.ShotsOffGoal = parseInt;
+                    }
+
+                    double possesionPercent;
+                    if(double.TryParse(values[7], out possesionPercent))
+                    {
+                        gameResult.PossessionPercent = possesionPercent;
+                    }
+
+                    soccerResults.Add(gameResult);
+                }
+            }
+            return soccerResults;
+
         }
     }
 }
